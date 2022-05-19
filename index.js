3d5a44302174f03c87982e46a27ecf0efd81481c
Taemin Kim <taeminkimny@gmail.com>
2022-05-19 10:21:10 -0400

    commit tree 4896b928f5275c15b1ec14760b3c145dd2207bce

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "a"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+function superbowlWin(record) {
+    const winners = record.find( ({result}) => result === 'W');
+    return (winners ? winners.year : undefined); 
+}
+
+console.log(superbowlWin(record))
